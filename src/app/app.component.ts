6a846eab54772e6dbcdf696e7301d6e63e1cdaff
BarathC <barathchitti@gmail.com>
2020-08-29 22:18:46 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {ResizableLayoutEnum} from './enums/app-enum';
 
-export type SelectOption = {id: ResizableLayoutEnum, value:string};
+export type SelectOption = {id: ResizableLayoutEnum, value:string, enable: boolean};
 
 @Component({
   selector: 'my-app',
@@ -12,10 +12,10 @@ export class AppComponent  {
   public eLayout: ResizableLayoutEnum = ResizableLayoutEnum.Right;
 
   public selectOptions: SelectOption[] = [
-    {id: ResizableLayoutEnum.Left, value: 'Left'},
-    {id:ResizableLayoutEnum.Right, value: 'Right'},
-    {id: ResizableLayoutEnum.Top, value: 'Top'},
-    {id: ResizableLayoutEnum.Bottom, value: 'Bottom'}
+    {id: ResizableLayoutEnum.Left, value: 'Left', enable: true},
+    {id:ResizableLayoutEnum.Right, value: 'Right', enable: true},
+    {id: ResizableLayoutEnum.Top, value: 'Top', enable: true},
+    {id: ResizableLayoutEnum.Bottom, value: 'Bottom', enable: false}
   ];
 
   public efnSelectedThread( oSelectedOption: SelectOption) {
